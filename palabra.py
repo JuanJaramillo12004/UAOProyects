9abaee73dc1e664e5a0807c9e4d656136d860791
JaramilloTR12 <142842480+JaramilloTR12@users.noreply.github.com>
2024-02-03 16:54:59 -0500

    Add files via upload

---
+++ palabra.py
@@ -0,0 +1,11 @@
+import random
+
+longitud = int(input("Longitud de la palabra: "))
+caracteres = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+palabra = ""
+otro = "S"
+while(otro == "S"):
+    for i in range(longitud):
+        palabra = palabra + random.choice(longitud)
+        print(palabra)
+    otro = input("Desea generar otra palabra? \nS=SI ó N=NO: ").upper()
