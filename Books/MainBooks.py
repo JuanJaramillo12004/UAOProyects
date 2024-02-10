45939dc616057e1616cfb5ff30141b982763e33d
JaramilloTR12 <juan.e.jara12004@gmail.com>
2024-02-10 11:05:09 -0500

    update

--- Books/MainBooks.py
+++ Books/MainBooks.py
@@ -6,14 +6,40 @@ MiBook.loadDataFromCSV(file)
 
 opcion = int(input("Cómo quiere filtrar? \n1=Lambda ó 2=List Comprehension: "))
 
+dato = int(input("""Dato por el que desea filtrar 
+1. Artistas 
+2. Titulo
+3. Año
+
+Digite 1,2 ó 3: """))
+
 if(opcion == 1):
-    dato = int(input("Dato por el que desea filtrar \n1. Artistas ó title ó year: "))
-    filtro = input(f"Ingrese el {dato} que quiere filtrar: ")
-    MiBook.filterBooks(dato,filtro) 
+    
+    if(dato == 1):
+        filtro = input(f"Ingrese el artista que quiere filtrar: ")
+        MiBook.filterBooks("artists",filtro)
+        
+    elif(dato == 2):
+        filtro = input(f"Ingrese el titulo que quiere filtrar: ")
+        MiBook.filterBooks("title",filtro)
+        
+    else:
+        filtro = input(f"Ingrese el año que quiere filtrar: ")
+        MiBook.filterBooks("year",filtro)
+        
 elif(opcion == 2):
-    dato = input("Dato por el que desea filtrar \nartists ó title ó year: ").lower()
-    filtro = input(f"Ingrese el {dato} que quiere filtrar: ")
-    MiBook.filterBooks2(dato,filtro)
+    
+    if(dato == 1):
+        filtro = input(f"Ingrese el artista que quiere filtrar: ")
+        MiBook.filterBooks2("artists",filtro)
+        
+    elif(dato == 2):
+        filtro = input(f"Ingrese el titulo que quiere filtrar: ")
+        MiBook.filterBooks2("title",filtro)
+        
+    else:
+        filtro = input(f"Ingrese el año que quiere filtrar: ")
+        MiBook.filterBooks2("year",filtro)
 else:
     print("Opción incorrecta")
 
