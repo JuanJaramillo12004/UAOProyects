c12288872be97cf957a785e0f6e1f3e029254575
JaramilloTR12 <juan.e.jara12004@gmail.com>
2024-04-16 10:22:11 -0500

    update

--- EDyA1/ProyectoEDyA/Scripts/utils.js
+++ EDyA1/ProyectoEDyA/Scripts/utils.js
@@ -1,13 +1,33 @@
 let input = document.getElementById("inputString");
-let btnReporte = document.getElementById("reporte");
+document.getElementById("reporte").addEventListener("click", desplegar);
 let tArea = document.getElementById("respuesta");
 document.getElementById("ingresar").addEventListener("click", function(){calcularScoreBoard(input.value);});
+let opcion = document.getElementById("criterio");
 
 function calcularScoreBoard(caso){
     tArea.value = "";
     caso.split(";").forEach((elemento) => {
         let resultado = elemento.split(" ");
+        resultado[1] = parseInt(resultado[1]);
+        resultado.sort((a, b) => a[1] - b[1]);
+        console.log(resultado); 
         tArea.value += `${resultado[0]} ${resultado[1]} ${resultado[2]} ${resultado[3]}\n`;
     });
-    console.log(res);
+    console.log(tArea.value);
+}
+
+function desplegar(){
+    if(opcion.value == "problemsSolved"){
+        tArea.value = tArea.value.split('\n').filter(Boolean).sort((a, b) => {
+            const scoreA = parseInt(a.split(' ')[1]);
+            const scoreB = parseInt(b.split(' ')[1]);
+            return scoreB - scoreA;
+        }).join('\n');
+    } else if(opcion.value == "penaltyTime"){
+        tArea.value = tArea.value.split('\n').filter(Boolean).sort((a, b) => {
+            const scoreA = parseInt(a.split(' ')[2]);
+            const scoreB = parseInt(b.split(' ')[2]);
+            return scoreA - scoreB;
+        }).join('\n');
+    }
 }
