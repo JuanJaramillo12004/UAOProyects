1d26a5ac7aca310fff0838b124766cb7787a75ff
JaramilloTR12 <juan.e.jara12004@gmail.com>
2024-04-24 15:57:24 -0500

    update

---
+++ SpringBoot/MongoBD2/src/main/java/com/example/MongoBD2/MongoBd2Application.java
@@ -0,0 +1,13 @@
+package com.example.MongoBD2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoBd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoBd2Application.class, args);
+	}
+
+}
---
+++ SpringBoot/MongoBD2/src/test/java/com/example/MongoBD2/MongoBd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MongoBD2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoBd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
