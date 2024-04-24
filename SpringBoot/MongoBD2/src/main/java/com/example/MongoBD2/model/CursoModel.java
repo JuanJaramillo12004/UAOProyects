843b64165e8ae9fbc6b09d8ee6f0137b3b8bafb4
JaramilloTR12 <juan.e.jara12004@gmail.com>
2024-04-24 16:02:15 -0500

    update

--- SpringBoot/MongoBD2/src/main/java/com/example/MongoBD2/model/CursoModel.java
+++ SpringBoot/MongoBD2/src/main/java/com/example/MongoBD2/model/CursoModel.java
@@ -13,5 +13,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 public class CursoModel {
     @Id
-    private Integer id;
+    private Integer id_curso;
+    private String nombre;
+    private String categoria;
+    private Integer total_horas;
 }
