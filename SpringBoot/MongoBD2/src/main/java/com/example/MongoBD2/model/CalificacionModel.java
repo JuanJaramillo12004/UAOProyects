5ccca3b8c994b06ad1b198250992cfd88896df8d
JaramilloTR12 <juan.e.jara12004@gmail.com>
2024-04-24 16:04:22 -0500

    i

--- SpringBoot/MongoBD2/src/main/java/com/example/MongoBD2/model/CalificacionModel.java
+++ SpringBoot/MongoBD2/src/main/java/com/example/MongoBD2/model/CalificacionModel.java
@@ -1,4 +1,17 @@
 package com.example.MongoBD2.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Calificaciones")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CalificacionModel {
+    @Id
+
 }
